c87e558e510ed6f7c7a46a1436a3e27fe3b5ce9d
yousef <yousef.elkholy2022@gmail.com>
2023-04-05 23:00:31 +0200

    First push

---
+++ JavaApplication69/src/javaapplication69/JavaApplication69.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication69;
+
+/**
+ *
+ * @author yousef
+ */
+public class JavaApplication69 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
